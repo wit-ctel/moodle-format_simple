db0edbc78cc3d25158637fe0e2d5ac2724b57f2e
Cathal O'Riordan <coriordan@wit.ie>
2014-09-23 13:18:49 +0100

    apply course-image class

--- renderer.php
+++ renderer.php
@@ -389,6 +389,7 @@ class format_simple_renderer extends format_section_renderer_base {
             $course_image_url = $this->output->pix_url('no-course-img', 'format_simple'); // then use default image
         }
         
-        return html_writer::empty_tag('img', array('src' => $course_image_url, 'alt' => 'Course Image'));
+        return html_writer::empty_tag('img', array('src' => $course_image_url, 
+                                                   'class' => 'course-image', 'alt' => 'Course Image'));
     }
 }
