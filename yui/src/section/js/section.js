1a2ba9e901eed99ac958144068a495592a46aa74
Cathal O'Riordan <coriordan@wit.ie>
2014-10-14 13:05:02 +0100

    initial commit of yui directory

---
+++ yui/src/section/js/section.js
@@ -0,0 +1,29 @@
+/**
+ * Section
+ *
+ * @class M.format_simple.section
+ * @constructor
+ * 
+ */
+
+// The CSS classes we use.
+var CSS = {
+
+    },
+    // The CSS selectors we use.
+    SELECTOR = {
+      
+    }; 
+
+// Setup the basic namespace.
+M.format_simple.section = M.format_simple.section || {};
+
+
+var SECTION = function() {
+    SECTION.superclass.constructor.apply(this, arguments);
+};
+
+Y.extend(SECTION, Y.Base, {
+
+}
+);  
